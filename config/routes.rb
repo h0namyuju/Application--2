dd22ad71f3eadc338b24c6924f9a391bfab2003e
h0namyuju <yuanju0h@gmail.com>
2022-03-31 11:36:00 +0000

    [Add]10章

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as:'about'
   
-  resources post_image, only: [:new, :index, :show]
+  resources post_image, only: [:new, :index, :show, :s]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
