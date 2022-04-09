b44fca510374b971ff8080f72dfbb295dab31675
h0namyuju <yuanju0h@gmail.com>
2022-04-09 13:17:17 +0000

    [ADD]18章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def updated
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path
   end
 end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
